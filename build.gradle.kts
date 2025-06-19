d8b22d0e278d248e7f9ed09b12fe60071f0804e5
Sleypner <sleypnersaf@gmail.com>
2025-06-19 18:23:19 +0300

    Fix Manifold connections. Change build java version to 21 and grouping dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ tasks.withType<Test> {
 
 dependencies {
 
+    // Spring Boot Starters
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf:3.3.1")
@@ -40,39 +41,56 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator:3.3.1")
     implementation("org.springframework.boot:spring-boot-starter-security:3.3.1")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client:3.3.4")
-    implementation("org.springframework.boot:spring-boot-devtools:3.3.2")
     implementation("org.springframework.boot:spring-boot-starter-cache:3.3.2")
     implementation("org.springframework.boot:spring-boot-starter-validation:3.5.0")
-    implementation("org.springframework.boot:spring-boot-devtools:3.3.1")
-    implementation("org.springframework.boot:spring-boot-starter-mail:3.3.1")
     implementation("org.springframework.boot:spring-boot-starter-webflux:3.5.0")
+
+    // Spring Boot DevTools
+    implementation("org.springframework.boot:spring-boot-devtools:3.3.2")
+
+    // OpenAPI / Swagger UI
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
 
-
+    // Hibernate and Database Drivers
     implementation("org.hibernate.orm:hibernate-community-dialects:7.0.0.Alpha3")
     implementation("org.xerial:sqlite-jdbc:3.46.0.0")
+    implementation("com.mysql:mysql-connector-j:9.0.0")
+    implementation("org.mariadb.jdbc:mariadb-java-client:3.4.1")
+    implementation("org.postgresql:postgresql:42.7.5")
+
+    // Thymeleaf
+    implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6:3.1.2.RELEASE")
+
+    // JSON / XML
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.17.1")
     implementation("org.jsoup:jsoup:1.15.3")
-    implementation("com.mysql:mysql-connector-j:9.0.0")
-    implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6:3.1.2.RELEASE")
-    implementation("org.mariadb.jdbc:mariadb-java-client:3.4.1")
+
+    // Logging
     implementation("net.logstash.logback:logstash-logback-encoder:8.0")
+
+    // Manifold
     implementation("systems.manifold:manifold-ext-rt:2025.1.20")
     annotationProcessor("systems.manifold:manifold-ext:2025.1.20")
     compileOnly("systems.manifold:manifold-all:2025.1.20")
 
-    implementation("org.postgresql:postgresql:42.7.5")
-
+    // Utility
     implementation("com.github.ua-parser:uap-java:1.6.1")
 
-//	implementation("org.jobrunr:jobrunr-spring-boot-3-starter:7.3.2")
+    // Lombok
     compileOnly("org.projectlombok:lombok:1.18.36")
     annotationProcessor("org.projectlombok:lombok:1.18.36")
+
+    // Resilience4j
     implementation("io.github.resilience4j:resilience4j-reactor:2.3.0")
     implementation("io.github.resilience4j:resilience4j-ratelimiter:2.3.0")
 
-    //DeepSeekClient
+    // OpenAI Java SDK
     implementation("com.openai:openai-java:2.8.1")
+
+    // Testing
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+
+    // implementation("org.jobrunr:jobrunr-spring-boot-3-starter:7.3.2")
 }
 

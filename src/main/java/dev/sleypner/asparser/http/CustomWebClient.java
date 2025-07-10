2b2d8df8910e08642ad334655d8efb29d0c023b5
Sleypner <sleypnersaf@gmail.com>
2025-07-10 15:54:14 +0300

    Added saving images by URI in CustomWebClient and limited the maximum number of connections to one.

--- src/main/java/dev/sleypner/asparser/http/CustomWebClient.java
+++ src/main/java/dev/sleypner/asparser/http/CustomWebClient.java
@@ -1,5 +1,6 @@
 package dev.sleypner.asparser.http;
 
+import dev.sleypner.asparser.domain.model.Image;
 import dev.sleypner.asparser.util.HtmlDocument;
 import io.github.resilience4j.ratelimiter.RateLimiter;
 import io.github.resilience4j.ratelimiter.RateLimiterConfig;
@@ -7,12 +8,21 @@ import io.github.resilience4j.reactor.ratelimiter.operator.RateLimiterOperator;
 import org.jsoup.Jsoup;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
 import reactor.util.retry.Retry;
 
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.Duration;
 
 public class CustomWebClient {
@@ -20,17 +30,22 @@ public class CustomWebClient {
     Logger log = LoggerFactory.getLogger(CustomWebClient.class);
 
     private final WebClient webClient;
-    private final RateLimiter rateLimiter = RateLimiter.of(
-            "api-rate-limiter",
+    private final RateLimiter rateLimiter = RateLimiter.of("api-rate-limiter",
             RateLimiterConfig.custom()
                     .limitForPeriod(2)
                     .limitRefreshPeriod(Duration.ofSeconds(1))
-                    .timeoutDuration(Duration.ofMillis(500))
+                    .timeoutDuration(Duration.ofSeconds(10))
                     .build()
     );
 
     public CustomWebClient(String baseUrl) {
+
+        HttpClient httpClient = HttpClient.create(ConnectionProvider.builder("custom")
+                .maxConnections(1)
+                .build());
+
         this.webClient = WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .baseUrl(baseUrl)
                 .defaultHeader(HttpHeaders.USER_AGENT, "Mozilla/5.0")
                 .build();
@@ -46,20 +61,61 @@ public class CustomWebClient {
                 });
     }
 
-    private Mono<String> fetchHtmlPage(String url) {
-        log.info("Fetching page {}", url);
-        return webClient.get()
-                .uri(url)
-                .retrieve()
-                .onStatus(
-                        status -> status == HttpStatus.TOO_MANY_REQUESTS,
-                        response -> Mono.error(new RuntimeException("Rate limit exceeded"))
-                )
-                .bodyToMono(String.class)
-                .transformDeferred(RateLimiterOperator.of(rateLimiter))
+    public Mono<Image> fetchImages(Image image) {
+        String uri = image.getExternalUri();
+        Path dir = Path.of(image.getDir());
+        Path path = Path.of(image.getPath());
+
+        Mono<Void> createDirsMono = Mono.fromCallable(() -> {
+            Files.createDirectories(dir);
+            return null;
+        }).subscribeOn(Schedulers.boundedElastic()).then();
+
+        String pattern = "*" + image.getExternalName();
+
+        return fileExists(dir, pattern)
+                .flatMap(exists -> {
+                    if (exists) {
+                        log.info("File already exists: {}", path);
+                        return Mono.just(image);
+                    } else {
+                        return Mono.defer(() ->
+                                webClient.get()
+                                        .uri(uri)
+                                        .retrieve()
+                                        .bodyToMono(DataBuffer.class)
+                                        .flatMap(dataBuffer ->
+                                                createDirsMono.then(DataBufferUtils.write(Mono.just(dataBuffer), path))
+                                                        .thenReturn(image)
+                                        )
+                        ).transformDeferred(RateLimiterOperator.of(rateLimiter));
+                    }
+                });
+    }
+
+    private Mono<String> fetchHtmlPage(String uri) {
+        log.info("Fetching page {}", uri);
+
+        return Mono.defer(() ->
+                        webClient.get()
+                                .uri(uri)
+                                .retrieve()
+                                .onStatus(
+                                        status -> status == HttpStatus.TOO_MANY_REQUESTS,
+                                        response -> Mono.error(new RuntimeException("Rate limit exceeded"))
+                                )
+                                .bodyToMono(String.class)
+                ).transformDeferred(RateLimiterOperator.of(rateLimiter))
                 .retryWhen(Retry.backoff(3, Duration.ofSeconds(10))
                         .filter(ex -> ex instanceof RuntimeException &&
-                                ex.getMessage().contains("Rate limit exceeded"))
-                );
+                                ex.getMessage().contains("Rate limit exceeded")));
+    }
+
+    public Mono<Boolean> fileExists(Path dir, String pattern) {
+        return Mono.fromCallable(() -> {
+            try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir, pattern)) {
+                return stream.iterator().hasNext();
+            }
+        }).subscribeOn(Schedulers.boundedElastic());
     }
 }
